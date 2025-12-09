63387dd8fe7caf6233c53ce153abd6c92e727fe2
edavidsmith <eliasdavidsmith52@gmail.com>
2025-12-09 10:00:12 -0600

    Add files

---
+++ logic.py
@@ -0,0 +1,103 @@
+from gui import *
+from PyQt6.QtWidgets import *
+import csv
+
+class Logic(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.submitincome_button.clicked.connect(self.submit_income)
+        self.expensesubmit_button.clicked.connect(self.submit_expense)
+        self.get_budget_amount()
+
+    def get_budget_amount(self):
+        budget_amount = 0.0
+        with open("budgetdata.csv", "r", newline="") as csvfile:
+            csvreader = csv.reader(csvfile)
+            for line in csvreader:
+                try:
+                    budget_amount = float(line[-1])
+                except ValueError:
+                    budget_amount = 0.0
+
+        self.balance_label.setText(f"${budget_amount:.2f}")
+
+    def submit_income(self):
+        try:
+            if self.hourlywage_entry.text() and self.hoursworked_entry.text():
+                wage_calculation = float(self.hourlywage_entry.text()) * float(self.hoursworked_entry.text())
+                current_total = 0.0
+                with open("budgetdata.csv", "r", newline="") as csvfile:
+                    csvreader = csv.reader(csvfile)
+                    for line in csvreader:
+                        try:
+                            current_total = float(line[-1])
+                        except ValueError:
+                            current_total = 0.0
+
+                current_total += wage_calculation
+
+                with open("budgetdata.csv", "a", newline="") as csvfile:
+                    row = ["Income", f"{wage_calculation:.2f}", "Income", f"{current_total:.2f}"]
+                    csvwriter = csv.writer(csvfile)
+                    csvwriter.writerow(row)
+
+                self.hourlywage_entry.clear()
+                self.hoursworked_entry.clear()
+            elif self.howmuchincome_entry.text():
+                total_income = float(self.howmuchincome_entry.text())
+                current_total = 0.0
+                with open("budgetdata.csv", "r", newline="") as csvfile:
+                    csvreader = csv.reader(csvfile)
+                    for line in csvreader:
+                        try:
+                            current_total = float(line[-1])
+                        except ValueError:
+                            current_total = 0.0
+
+                current_total += total_income
+
+                with open("budgetdata.csv", "a", newline="") as csvfile:
+                    row = ["Income", f"{total_income:.2f}", "Income", f"{current_total:.2f}"]
+                    csvwriter = csv.writer(csvfile)
+                    csvwriter.writerow(row)
+                self.howmuchincome_entry.clear()
+
+            budget_amount = 0.0
+            with open("budgetdata.csv", "r", newline="") as csvfile:
+                csvreader = csv.reader(csvfile)
+                for line in csvreader:
+                    try:
+                        budget_amount = float(line[-1])
+                    except ValueError:
+                        budget_amount = 0.0
+
+            self.balance_label.setText(f"${budget_amount:.2f}")
+            self.budget_message.setText("Report A Change To Your Budget")
+        except (ValueError, TypeError):
+            self.budget_message.setText("Please Only Enter Numeric Values")
+
+    def submit_expense(self):
+        try:
+            current_total = 0.0
+            expense_amount = float(self.amountofexpense_entry.text())
+            with open("budgetdata.csv", "r", newline="") as csvfile:
+                csvreader = csv.reader(csvfile)
+                for line in csvreader:
+                    try:
+                        current_total = float(line[-1])
+                    except ValueError:
+                        current_total = 0.0
+
+            current_total -= expense_amount
+            expense_reason = self.expensetype_dropbox.currentText()
+
+            with open("budgetdata.csv", "a", newline="") as csvfile:
+                row = ["Expense", f"-{expense_amount:.2f}", expense_reason, f"{current_total:.2f}"]
+                csvwriter = csv.writer(csvfile)
+                csvwriter.writerow(row)
+
+            self.balance_label.setText(f"${current_total:.2f}")
+            self.budget_message.setText("Report A Change To Your Budget")
+        except (ValueError, TypeError):
+            self.budget_message.setText("Please Only Enter Numeric Values")
